a4208c47a983210c7c0882bb4a09207633664291
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-09-09 14:28:46 +0700

    fixed flake8

--- statistics.py
+++ statistics.py
@@ -10,7 +10,7 @@ def average(data):
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """The population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -41,5 +41,6 @@ def variance(data):
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """Return the population standard deviation of a list
+    of numbers in data."""
     return sqrt(variance(data))
